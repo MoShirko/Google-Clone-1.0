978640ae5a76890bcbef8d3234c72b9336177fc9
shirko <shir617@gmail.com>
2023-08-01 14:02:03 -0400

    footer update

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,23 @@ import { GlobeIcon } from "@heroicons/react/solid";
 
 function Footer() {
   return (
-    <footer>
-      <div>
-        <p>hello</p>
+    <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
+      <div className="px-8 py-3">hello</div>
+      <div className="grid grid-cols-1 md:gird-col-2 lg:grid-col-3 gap-y-4 px-8 py-3 grid-flow-row-dense">
+        <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2">
+          <GlobeIcon className="h-5 mr-1 text-green-700" />
+          Our third decade of climate action: join us
+        </div>
+        <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
+          <p>Advertising</p>
+          <p>Business</p>
+          <p>How Search works</p>
+        </div>
+        <div className="flex justify-center space-x-8 md:ml-auto">
+          <p>Privacy</p>
+          <p>Terms</p>
+          <p>Settings</p>
+        </div>
       </div>
     </footer>
   );
