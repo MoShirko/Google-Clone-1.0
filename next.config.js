3f6f8f66f109ca9e8f929aaecda2cf97de2e6e99
shirko <shir617@gmail.com>
2023-08-01 11:38:50 -0400

    update changes from old repo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com',"www.pngall.com"],
+  },
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
       <div className="flex flex-col w-1/2 space-y-2 justify-center 
       mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
         <button className="btn">Google Search</button>
-        <button className="btn">I'm Feeling Lucky</button>
+        <button className="btn">Im Feeling Lucky</button>
       </div>
       </form>
      
