724dbfc40228174af729937585434169387d565c
Molik Kidd <mzkidd5@gmail.com>
2023-08-03 14:53:47 -0400

    add header option and icons

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,9 +1,46 @@
-import React from 'react'
-
+import {
+  DotsVerticalIcon,
+  MapIcon,
+  NewspaperIcon,
+  PhotographIcon,
+  PlayIcon,
+  SearchIcon,
+} from "@heroicons/react/outline";
+import React from "react";
+import HeaderOption from "./HeaderOption";
 function HeaderOptions() {
   return (
-    <div>HeaderOptions</div>
-  )
+    <div className="flex w-full text-gray-700 justify-evenly text-sm 
+    lg:text-base lg:justify-start lg:space-x-36 lg:pl-52
+    border-b-[1px]">
+      {/* Left */}
+      <div className="flex space-x-6">
+        <HeaderOption Icon={SearchIcon} 
+        title="All"
+        />
+        <HeaderOption Icon={PhotographIcon} 
+        title="Images"
+        />
+        <HeaderOption Icon={PlayIcon}
+        title="Videos"
+        />
+        <HeaderOption Icon={NewspaperIcon}
+        title="News"
+        />
+        <HeaderOption Icon={MapIcon} 
+        title="Maps"
+        />
+        <HeaderOption Icon={DotsVerticalIcon} 
+        title="More"
+        />
+      </div>
+      {/* Right */}
+      <div className="flex space-x-4">
+        <p className="link">Settings</p>
+        <p className="link">Tools</p>
+      </div>
+    </div>
+  );
 }
 
-export default HeaderOptions
+export default HeaderOptions;
