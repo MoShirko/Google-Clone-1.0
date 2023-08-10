1a90228d54acea18267db94337cbf917f98c5754
Molik Kidd <mzkidd5@gmail.com>
2023-08-10 12:54:09 -0400

    add google search engine api

--- keys.js
+++ keys.js
@@ -1,8 +1,4 @@
 // API key https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = 'YOUR_API_KEY_HERE';
+// export const API_KEY = '';
 // Context key - https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = '2028067edeb7a487a';
-
-{/* <script async src="https://cse.google.com/cse.js?cx=2028067edeb7a487a">
-</script>
-<div class="gcse-search"></div> */}
+// export const CONTEXT_KEY = '2028067edeb7a487a';
--- pages/search.js
+++ pages/search.js
@@ -2,7 +2,8 @@ import React from 'react'
 import Head from "next/head";
 import Header from "../components/Header";
 
-function Search() {
+function Search({results}) {
+  console.log(results);
   return (
     <div>
         <Head>
@@ -20,7 +21,13 @@ export default Search;
 export async function getServerSideProps(context) {
   const useDummyData = false;
   
+  const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}`)
+  .then((response)=> response.json());
+  console.log(data);
+
   return {
-    props: {},
+    props: {
+      results: data,
+    },
   }
 }
