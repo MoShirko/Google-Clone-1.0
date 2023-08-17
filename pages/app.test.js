e865073180b8fdd3487078d5ad76c87f50899691
shirko <shir617@gmail.com>
2023-08-17 16:26:58 -0400

    test file update

--- pages/app.test.js
+++ pages/app.test.js
@@ -1,21 +1,25 @@
 import React from 'react';
-// import '@testing-library/jest-dom';
-import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ReactDOM from 'react-dom';
+// import { render, screen, fireEvent } from '@testing-library/react';
 import App from "./_app";
 import HeaderOptions from "../components/HeaderOptions";
+import Header from "../components/Header";
 import HeaderOption from "../components/HeaderOption";
 import index from "./index"
+import { render, screen, fireEvent } from "@testing-library/jest-dom/extend-expect"
 /**
  * Verify something should render
  */
  test('App should render', () => {
-    render(<HeaderOptions/>);
-    expect(screen.getByRole('toolbar')).toHaveTextContent('settings');
+  ReactDOM.render(<HeaderOptions/>);
+    const pTag =  screen.getByRole('toolbar')
+    expect(pTag).toContainHTML('Settings');
   });
 
-  // test('App should render', () => {
-  //   render(<Header/>);
-  //   const header = new Header;
-  //   const searchIcon = screen.getByRole('searchIcon');
-  //   expect(header).toContain(searchIcon);
-  // });
+  test('App should render', () => {
+    ReactDOM.render(<Header/>);
+    const header = new Header;
+    const searchIcon = screen.getByRole('searchIcon');
+    expect(header).toContain(searchIcon);
+  });
