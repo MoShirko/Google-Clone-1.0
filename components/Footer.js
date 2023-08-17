5a82835d69322476cd33a7f16d7cb4f5da83fe41
shirko <shir617@gmail.com>
2023-08-17 12:57:02 -0400

    integrating accessiblity

--- components/Footer.js
+++ components/Footer.js
@@ -5,19 +5,19 @@ function Footer() {
   return (
     <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
       <div className="grid grid-cols-1 md:gird-col-2 lg:grid-col-3 xl:grid-col-3 gap-y-4 px-8 py-3 grid-flow-row-dense">
-        <div className="flex justify-center items-center md:col-span-2 xl:col-span-3 xl:px-0 hover:underline cursor-pointer">
-          <GlobeIcon className="h-5 mr-1 text-green-700" />
+        <div tabindex="0" className="flex justify-center items-center md:col-span-2 xl:col-span-3 xl:px-0 hover:underline cursor-pointer">
+          <GlobeIcon tabindex="0" className="h-5 mr-1 text-green-700" />
           Our third decade of climate action: join us
         </div>
         <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
-          <p className="hover:underline cursor-pointer">Advertising</p>
-          <p className="hover:underline cursor-pointer">Business</p>
-          <p className="hover:underline cursor-pointer">How Search works</p>
+          <p tabindex="0" className="hover:underline cursor-pointer">Advertising</p>
+          <p tabindex="0" className="hover:underline cursor-pointer">Business</p>
+          <p tabindex="0" className="hover:underline cursor-pointer">How Search works</p>
         </div>
         <div className="flex justify-center space-x-8 md:ml-auto">
-          <p className="hover:underline cursor-pointer">Privacy</p>
-          <p className="hover:underline cursor-pointer">Terms</p>
-          <p className="hover:underline cursor-pointer">Settings</p>
+          <p tabindex="0" className="hover:underline cursor-pointer">Privacy</p>
+          <p tabindex="0" className="hover:underline cursor-pointer">Terms</p>
+          <p tabindex="0" className="hover:underline cursor-pointer">Settings</p>
         </div>
       </div>
     </footer>
