6b3ff2cded4afb0c516d694b219fc3beea852230
Molik Kidd <mzkidd5@gmail.com>
2023-08-24 15:21:23 -0400

    add page extensions for testing

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com',"www.pngall.com","www.camo.githubusercontent.com", "www.freepngimg.com","www.static-00.iconduck.com", "upload.wikimedia.org","www.cdn.icon-icons.com", "www.assets.com"],
   },
+  pageExtensions: [pages.js]
 }
 
 module.exports = nextConfig
